4f0c5f614b6ad247e54fcb14e04fd72be3bf9728
MOUSTAMID <karim1992moustamid@gmail.com>
2020-08-31 22:10:13 +0100

    adding the sorting the the table

--- src/App.js
+++ src/App.js
@@ -1,26 +1,132 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import { 
+  MenuItem ,
+  FormControl ,
+  Select,
+  Card ,
+  CardContent
+} from "@material-ui/core" ;
+
+import InfoBox from './components/InfoBox';
+import Map from './components/map';
+import Table from './components/table/Table';
+import {sortData} from './Helper/util';
+
+
+
+// Api link : https://disease.sh/v3/covid-19/jhucsse
+
+// USEEFFECT = Runs a piece of code based on a given condition  : 
+    //  The code inside the USEEFFECT runs once ,
+    // when the component loads and not again only if the condition [] is met .
+
+
 function App() {
+  
+  // states are how we write varibles in react :
+  const  [countries , setCountries] = useState( [] ) ;
+  const  [country , setContry ] = useState("worldwide") ;
+  const  [countryInfo , setContryInfo ] = useState({}) ;
+  const  [TableData , setTableData ] = useState([]) ;
+
+useEffect( ()=> {
+    fetch("https://disease.sh/v3/covid-19/all")
+   .then(respense => respense.json())
+   .then(data => {
+    setContryInfo(data);
+   });
+
+} , [] );  
+  
+useEffect( ()=>{
+  // async -> send a requiste to the server , wait for it , do somethinf with the responce . 
+  const getCountriesData = async () => {
+    await fetch( "https://disease.sh/v3/covid-19/countries" )
+    .then( respense => respense.json() )
+    .then( data => {
+       const countries = data.map( country => (
+            {
+              name : country.country ,
+              value : country.countryInfo.iso3 
+            }
+       ));
+       
+       const  sortedData = sortData(data);
+       setTableData(sortedData);
+       setCountries(countries);
+
+    });
+
+  };
+
+  getCountriesData();
+
+} , [] );
+
+
+const onCountryChange = async e => {
+   const countryCode = e.target.value ;
+
+   //  https://disease.sh/v3/covid-19/all
+  //  https://disease.sh/v3/covid-19/countries/[CPUNTRY_CODE]
+  
+  const url = countryCode === 'worldwide' 
+        ? 'https://disease.sh/v3/covid-19/all'
+        :  `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+   
+        await fetch(url)
+        .then(response => response.json() )
+        .then( data => {
+          setContry(countryCode);
+          setContryInfo(data);
+          console.log(countryCode);
+          console.log(data);
+        });
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+
+    <div className="app">
+      <div className="app__left">
+      <div className="app__header">
+        <h1>Covid-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+        <Select
+         variant="outlined" 
+         value={country} 
+         onChange={onCountryChange}
         >
-          Learn React
-        </a>
-      </header>
+        <MenuItem value="worldwide" >worldwide</MenuItem> 
+         { countries.map( (country)=>(
+              <MenuItem value={country.value} >{country.name}</MenuItem>
+         ) ) }
+
+        </Select> 
+        </FormControl>   
+      </div>
+  
+
+      <div className="app__stats">
+      <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases} />
+      <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered} />
+      <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
+      </div>
+
+      {/* map */}
+        <Map/>
+      </div>
+       <Card className="app__right">
+         <CardContent>
+           <h3>Live Cases by Country</h3>
+           <Table countries={TableData} />
+           <h3>Wordwide new cases</h3>  
+         </CardContent>
+       </Card>
     </div>
   );
 }
-
+ 
 export default App;
---
+++ src/components/map.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Map() {
+  return (
+    <div className="map">
+      {
+        <h1>I am the Map</h1>
+      }
+    </div>
+  )
+}
+
+export default Map;
+ 
